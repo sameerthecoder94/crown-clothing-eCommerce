708e7921c55ab439e557e58667aeed7c5d9536e0
sameerthecoder94 <sameerthecoder@gmail.com>
2022-05-06 09:09:19 -0400

    Navigation Bar Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Home from './routes/home/home.component';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>I am a Navigation Bar</div>
+      <Outlet />
+    </div>
+  );
+};
 
 const Shop = () => {
   return (
@@ -12,7 +21,8 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />}>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
