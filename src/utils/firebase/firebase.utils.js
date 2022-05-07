8cd351f0e573995a5f8158f18a75314b783aa15c
sameerthecoder94 <sameerthecoder@gmail.com>
2022-05-07 17:35:51 -0400

    `Finished Creating User Documents` added sign-in component, firebase utils, brought in firebase libraries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -28,9 +28,25 @@ export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, 'users', userAuth.uid);
-  console.log(userDocRef);
 
   const userSnapshot = await getDoc(userDocRef);
-  console.log(userSnapshot);
-  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    // if user does not exist in db, create new user doc
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        // create new user doc in db with userAuth data and createdAt date
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
 };
