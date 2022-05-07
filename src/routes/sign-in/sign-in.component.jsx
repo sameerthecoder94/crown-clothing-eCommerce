ea863853616e3a07e4612a69e13fbbc30b8464d6
sameerthecoder94 <sameerthecoder@gmail.com>
2022-05-07 18:23:59 -0400

    only used `signInWithPopup` &, removed signInWithRedirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,24 +1,9 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
 import {
-  auth,
   signInWithGooglePopup,
-  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
-  useEffect(() => {
-    const fetchRedirectedData = async () => {
-      const response = await getRedirectResult(auth);
-      //   console.log(response);
-      if (response) {
-        createUserDocumentFromAuth(response.user);
-      }
-    };
-    fetchRedirectedData();
-  }, []);
-
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     createUserDocumentFromAuth(user);
@@ -28,9 +13,6 @@ const SignIn = () => {
     <div>
       <h1>Sign In page</h1>
       <button onClick={logGoogleUser}>Sign In with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign In with Google Redirect
-      </button>
     </div>
   );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,10 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import {
-  getAuth,
-  signInWithRedirect,
-  signInWithPopup,
-  GoogleAuthProvider,
-} from 'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -24,8 +19,6 @@ googleProvider.setCustomParameters({ prompt: 'select_account' });
 export const auth = getAuth();
 export const signInWithGooglePopup = () =>
   signInWithPopup(auth, googleProvider);
-export const signInWithGoogleRedirect = () =>
-  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
